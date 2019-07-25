c51589e890661c5a579e2a0e12f5fe60697672f1
AjayaSharma <ajaya.sharma@outlook.com>
2019-07-25 10:06:36 +0545

    Single card created with single news item from articleObject

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  // CREATE A DUMMY ARTICLE OBJECT - COPY THE DATA FROM NEWSAPI EXAMPLE 
+  articleObject = {
+    "source": {
+      "id": null,
+      "name": "Yahoo.com"
+    },
+    "author": "Natalie Neysa Alund",
+    "title": "Two dead, 768 ill amid Salmonella outbreak affecting 48 states, CDC says - Yahoo News",
+    "description": "The CDC reports two separate outbreaks – one linked to contact with poultry in backyard flocks and one linked to contact with pig ear dog treats.",
+    "url": "https://news.yahoo.com/two-dead-768-ill-amid-011759301.html",
+    "urlToImage": "https://s.yimg.com/os/mit/media/m/social/images/social_default_logo-1481777.png",
+    "publishedAt": "2019-07-25T01:32:57Z",
+    "content": "NASHVILLE, Tenn. Two people are dead and 768 are ill due to a nationwide Salmonella outbreak, the Centers for Disease Control and Prevention reported.\r\nThe CDC recently posted updates regarding  two separate outbreaks one linked to contact with poultry in bac… [+1258 chars]"
+  }
+  
   constructor() {}
 
 }
