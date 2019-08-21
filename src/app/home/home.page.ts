c0be2b39233e50ba89fd1021fd0d0dcc23d61c98
AjayaSharma <ajaya.sharma@outlook.com>
2019-08-21 19:33:02 +0545

    Adding pageloader

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,15 +7,20 @@ import { NewsApiService } from './../providers/news-api.service';
 })
 export class HomePage {
   articleList: Array<any> = [];
+  showPageLoader: boolean = false;
+
   constructor(private newsApiService: NewsApiService) {
     this.getTopHeadlines();
   }
 
   getTopHeadlines(){
+    this.showPageLoader = true;
     this.newsApiService.getTopHeadlines('us').subscribe((result: any) => {
       this.articleList = result.articles.filter(article => article.urlToImage);
+      this.showPageLoader = false;
     }, (error) => {
       console.log('error', error);
+      this.showPageLoader = false;
     });
   }
 }
