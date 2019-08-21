86755a9b32ef11b8610ae066f3275be8a1875408
AjayaSharma <ajaya.sharma@outlook.com>
2019-08-21 16:45:25 +0545

    Updated UI for news

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,14 +6,14 @@ import { NewsApiService } from './../providers/news-api.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  
+  articleList: Array<any> = [];
   constructor(private newsApiService: NewsApiService) {
     this.getTopHeadlines();
   }
 
   getTopHeadlines(){
-    this.newsApiService.getTopHeadlines('us').subscribe(result=> {
-      console.log('result', result);
+    this.newsApiService.getTopHeadlines('us').subscribe((result: any) => {
+      this.articleList = result.articles.filter(article => article.urlToImage);
     }, (error) => {
       console.log('error', error);
     });
